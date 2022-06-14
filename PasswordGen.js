a91cffa0702bd23fbf48eb518073729931ffdd56
Andrea <andrea.berrocal95@gmail.com>
2022-06-13 22:42:08 -0700

    added functions to get password generator working with button click

--- PasswordGen.js
+++ PasswordGen.js
@@ -3,22 +3,61 @@
 //consts with possibles characters
 //function randomizing possible characters/outputting a randomized password according to a password length
 
+
+//get elements needed
+let lengthElement = document.getElementById('length');
+let password = document.getElementById('final-password');
+const button = document.getElementById('btn');
+
+const lowerElement = document.getElementById('lower');
+const upperElement = document.getElementById('upper');
+const numberElement = document.getElementById('numbers');
+const specialElement = document.getElementById('special');
+
+
 const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
 const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const allTheLetters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "1234567890";
 const specialChars = "!@#$%^&*()_-";
 const everything = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_-"
+let str = '';
 
-function generatePassword(passwordLength, string){
+//function to generate needed chars according to boxes checked
+function generateNeededChars(){
+    if(lowerElement.checked){
+        str += lowerLetters;
+    }
+    if(upperElement.checked){
+        str += upperLetters;
+    }
+    if(numberElement.checked){
+        str += numbers;
+    }
+    if(specialElement.checked){
+        str += specialChars;
+    }
+    return str;
+}
+
+//function to generate password
+function generatePassword(length, str){
     let finalPassword = '';
-    for(let i = 0; i < passwordLength; i++){
-        let randomNumber = Math.floor(Math.random() * string.length);
-        finalPassword += string.charAt(randomNumber);
+    for(let i = 0; i < length; i++){
+        let randomNumber = Math.floor(Math.random() * str.length);
+        finalPassword += str.charAt(randomNumber);
     }
     return finalPassword;
 }
 
-const yourPassword = generatePassword(10, everything);
-console.log(`Your random password is: ${yourPassword}`);
+//event listeners
+button.addEventListener('click', function(){
+    let length = lengthElement.value
+    let str = generateNeededChars();
+    let finalPassword = generatePassword(length, str);
+    
+    password.textContent = finalPassword
+    password.style.textAlign = "center";
+
+});
 
